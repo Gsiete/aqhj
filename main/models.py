b30129ab2576b634074ae5417a68bc22b3f8f7a4
Gabriel Poussif <gpoussif@gmail.com>
2016-04-25 20:40:36 +0200

    CHANGE: update data structure

--- main/models.py
+++ main/models.py
@@ -1,13 +1,23 @@
 from django.conf import settings
 from django.db import models
-from timezone_field import TimeZoneField
+from cities.models import City
 
 
 class Tournament(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=50, default='')
-    start_date = models.DateTimeField('start of the tournament')
-    end_date = models.DateTimeField('end of the tournament')
+    logo = models.ImageField(upload_to='tournament/logo/')
+
+    def __str__(self):
+        return self.name
+
+
+class Season(models.Model):
+    tournament = models.ForeignKey(Tournament)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50, default='')
+    start = models.DateTimeField('start of the tournament')
+    end = models.DateTimeField('end of the tournament')
 
     def __str__(self):
         return self.name
@@ -15,7 +25,7 @@ class Tournament(models.Model):
 
 class Stadium(models.Model):
     name = models.CharField(max_length=200)
-    timezone = TimeZoneField(default='Europe/Madrid')
+    city = models.ForeignKey(City)
 
     def __str__(self):
         return self.name
@@ -26,6 +36,7 @@ class Team(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=50, default='')
     stadium = models.ForeignKey(Stadium)
+    logo = models.ImageField(upload_to='team/logo/')
     is_domain_team = models.BooleanField('is the main team of the domain')
 
     def __str__(self):
@@ -38,12 +49,27 @@ class Match(models.Model):
 
     team_a = models.ForeignKey(Team, on_delete=models.PROTECT, related_name='matches_as_a')
     team_b = models.ForeignKey(Team, on_delete=models.PROTECT, related_name='matches_as_b')
+    score_team_a = models.IntegerField(null=True, blank=True)
+    score_team_b = models.IntegerField(null=True, blank=True)
     stadium = models.ForeignKey(Stadium, on_delete=models.SET_NULL, null=True)
-    tournament = models.ForeignKey(Tournament, on_delete=models.SET_NULL, null=True)
+    season = models.ForeignKey(Season, on_delete=models.SET_NULL, null=True)
     time = models.DateTimeField('local time of the match')
 
     def stadium_time(self):
-        return self.time.astimezone(self.stadium.timezone)
+        return self.time.astimezone(self.stadium.city.timezone)
 
     def __str__(self):
         return str(self.team_a) + ' - ' + str(self.team_b) + ' (' + str(self.time) + ')'
+
+
+class TeamSeason(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
+    matches_played = models.IntegerField(null=True, blank=True)
+    wins = models.IntegerField(null=True, blank=True)
+    draws = models.IntegerField(null=True, blank=True)
+    losses = models.IntegerField(null=True, blank=True)
+    goals_for = models.IntegerField(null=True, blank=True)
+    goals_against = models.IntegerField(null=True, blank=True)
+    goals_difference = models.IntegerField(null=True, blank=True)
+    points = models.IntegerField(null=True, blank=True)
