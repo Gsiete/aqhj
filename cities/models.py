25ae6e5fe9c98d3ae71fbaf174369a3c2f0d9140
Gabriel Poussif <gpoussif@gmail.com>
2016-04-28 00:00:38 +0200

    ADD: add capital city to the country object

--- cities/models.py
+++ cities/models.py
@@ -1,7 +1,7 @@
 from cities_light.abstract_models import (AbstractCity, AbstractRegion, AbstractCountry)
 from cities_light.receivers import connect_default_signals
 import cities_light
-from cities_light.settings import ICity
+from cities_light.settings import ICity, ICountry
 from timezone_field import TimeZoneField
 from django.dispatch import receiver
 from django.db import models
@@ -9,7 +9,7 @@ import pytz
 
 
 class Country(AbstractCountry):
-    pass
+    capital = models.ForeignKey(City)
 connect_default_signals(Country)
 
 
@@ -29,3 +29,8 @@ connect_default_signals(City)
 @receiver(cities_light.signals.city_items_post_import)
 def set_city_fields(sender, instance, items, **kwargs):
     instance.timezone = pytz.timezone(items[ICity.timezone])
+
+
+@receiver(cities_light.signals.country_items_post_import)
+def process_country_import(sender, instance, items, **kwargs):
+    instance.capital = City.objects.get(name=items[ICountry.capital], country=instance)
