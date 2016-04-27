f0de1730bf3cd0b8ccdd5ab4d7cd46af18de7d71
Gabriel Poussif <gpoussif@gmail.com>
2016-04-27 23:07:52 +0200

    HACK: fall back to stadium city if no city is found (for now at least)

--- main/views.py
+++ main/views.py
@@ -7,5 +7,6 @@ from django.utils import timezone
 def index(request):
     next_match = Match.objects.filter(time__gte=timezone.now()).order_by('time')[0]
     user_city = aqhorajuega.functions.get_user_city(request)
+    user_city = user_city if user_city else next_match.stadium.city
 
     return render(request, 'main/index.html', {'next_match': next_match, 'user_city': user_city})
