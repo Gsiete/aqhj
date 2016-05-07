d895adcc2a91b2b76a981d655b19d22230464f13
Gabriel Poussif <gpoussif@gmail.com>
2016-05-07 15:21:57 +0200

    CHANGE: make time format a get variable

--- main/views.py
+++ main/views.py
@@ -8,5 +8,6 @@ def index(request):
     next_match = Match.objects.filter(time__gte=timezone.now()).order_by('time')[0]
     user_city = aqhorajuega.functions.get_user_city(request)
     user_city = user_city if user_city else next_match.stadium.city
+    time_format = request.GET.get('format', 24)
 
-    return render(request, 'main/index.html', {'next_match': next_match, 'user_city': user_city})
+    return render(request, 'main/index.html', {'time_format': time_format, 'next_match': next_match, 'user_city': user_city})
