bcd321a441fad858f85eb091cd1fe417b65e7e1d
Gabriel Poussif <gpoussif@gmail.com>
2016-05-11 23:34:26 +0200

    FIX: authentication shouldn't be required for Autocomplete

--- cities/views.py
+++ cities/views.py
@@ -4,10 +4,6 @@ from cities.models import City
 
 class CityAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
-        # Don't forget to filter out results depending on the visitor !
-        if not self.request.user.is_authenticated():
-            return City.objects.none()
-
         qs = City.objects.all()
 
         if self.q:
