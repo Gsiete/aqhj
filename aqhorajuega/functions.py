09d4452fa414e31a70560a765a95fb7024b749ec
Gabriel Poussif <gpoussif@gmail.com>
2016-05-13 04:46:27 +0200

    REFACTOR: handle timezone and time format related things in our own render function wrapping django.shortcuts' render

--- aqhorajuega/functions.py
+++ aqhorajuega/functions.py
@@ -1,7 +1,11 @@
 import random
 
 from django.contrib.gis.geoip2 import GeoIP2
+from django.shortcuts import render
 from geoip2.errors import AddressNotFoundError
+from django.conf import settings
+import datetime
+
 
 from cities.models import City, Country
 
@@ -41,6 +45,36 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+
+def aqhj_render(request, template, context):
+    city_code = request.COOKIES.get('city_code', None)
+    city_is_new = not city_code
+    user_city = get_user_city(request) if city_is_new else City.objects.get(pk=city_code)
+
+    time_format = request.COOKIES.get('tformat', '24')
+
+    context['time_format'] = time_format
+    context['user_city'] = user_city
+    context['city_is_new'] = city_is_new
+
+    response = render(request, template, context)
+
+    if city_is_new:
+        set_cookie(response, 'city_code', user_city.id)
+
+    return response
+
+
+def set_cookie(response, key, value, days_expire=None):
+    if days_expire is None:
+        max_age = 365 * 24 * 60 * 60  # one year
+    else:
+        max_age = days_expire * 24 * 60 * 60
+    expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age),
+                                         "%a, %d-%b-%Y %H:%M:%S GMT")
+    response.set_cookie(key, value, max_age=max_age, expires=expires, domain=settings.SESSION_COOKIE_DOMAIN,
+                        secure=settings.SESSION_COOKIE_SECURE or None)
 
 
 def get_random_ip(g):
