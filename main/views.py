e81a785be4f08a5c54d2af23babf93a76e919f96
Gabriel Poussif <gpoussif@gmail.com>
2016-05-21 21:39:11 +0200

    REFACTOR: use match as var name for main match in templates

--- main/views.py
+++ main/views.py
@@ -13,7 +13,7 @@ def index(request):
     following_matches = Match.objects.filter(time__gte=timezone.now()).order_by('time')[1:7]
 
     return aqhj_render(request, 'main/match-before.html',
-                       {'following_matches': following_matches, 'next_match': next_match, 'home': True})
+                       {'following_matches': following_matches, 'match': next_match, 'home': True})
 
 
 def match_before(request, **kwargs):
@@ -22,12 +22,12 @@ def match_before(request, **kwargs):
     if today:
         time_criteria['time__lte'] = timezone.now() + timedelta(1)
     kwargs.update(time_criteria)
-    next_match = get_object_or_404(Match, **kwargs)
-    # following_matches = Match.objects.filter(time__gte=next_match.time).order_by('time')[1:7]
-    following_matches = Match.objects.filter(~Q(id=next_match.id, time__lt=timezone.now())).order_by('time')[1:7]
+    match = get_object_or_404(Match, **kwargs)
+    # following_matches = Match.objects.filter(time__gte=match.time).order_by('time')[1:7]
+    following_matches = Match.objects.filter(~Q(id=match.id, time__lt=timezone.now())).order_by('time')[1:7]
 
     return aqhj_render(request, 'main/match-before.html',
-                       {'following_matches': following_matches, 'next_match': next_match})
+                       {'following_matches': following_matches, 'match': match})
 
 
 def past_match(request, **kwargs):
