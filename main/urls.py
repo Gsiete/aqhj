0d27f1baf1ab276dc1baa448b9b0dacf4648bba1
Gabriel Poussif <gpoussif@gmail.com>
2016-05-21 03:41:46 +0200

    ADD: last_matches view CHANGE: route for past match according to requirements

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,13 @@ from django.conf.urls import url
 
 from . import views
 
+season_pattern = r'(?P<season__tournament__slug>[\w-]+)/(?P<season__slug>[\w-]+)'
+match_pattern = season_pattern + r'/(?P<game_in_season>[\w-]+)/(?P<team_a__slug>[\w-]+)-vs-(?P<team_b__slug>[\w-]+)'
+past_match_pattern = match_pattern + r'-(?P<score_team_a>[0-9]+)-(?P<score_team_b>[0-9]+)/resultado-goles-y-noticias'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^past$', views.past_match, name='past_match'),
+    url(r'^' + past_match_pattern + r'$', views.past_match, name='past_match'),
+    url(r'^last-matches$', views.last_matches, name='last_matches'),
 ]
+# https://docs.djangoproject.com/en/1.9/topics/http/urls/#including-other-urlconfs
--- main/views.py
+++ main/views.py
@@ -2,16 +2,23 @@ from django.utils import timezone
 
 from main.functions import aqhj_render
 from main.models import Match
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
     next_match = Match.objects.filter(time__gte=timezone.now()).order_by('time')[0]
-    following_matches = [next_match for _ in range(5)]
+    following_matches = Match.objects.filter(time__gte=timezone.now()).order_by('time')[1:7]
 
     return aqhj_render(request, 'main/index.html', {'following_matches': following_matches, 'next_match': next_match})
 
 
-def past_match(request):
-    last_match = Match.objects.filter(time__lte=timezone.now()).order_by('-time')[0]
+def past_match(request, **kwargs):
+    last_match = get_object_or_404(Match, **kwargs)
 
     return aqhj_render(request, 'main/past-match.html', {'match': last_match})
+
+
+def last_matches(request):
+    last_matches = Match.objects.filter(time__lte=timezone.now()).order_by('-time')[:10]
+
+    return aqhj_render(request, 'main/last-matches.html', {'last_matches': last_matches})
