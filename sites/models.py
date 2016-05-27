9954d54797fcb71e2abd733d6c631f532de33315
Gabriel Poussif <gpoussif@gmail.com>
2016-05-27 22:08:56 +0200

    ADD: Route choices and other functionality

--- sites/models.py
+++ sites/models.py
@@ -1,30 +1,51 @@
-from django.core.urlresolvers import get_urlconf, get_resolver
+from django.conf import settings
 from django.db import models
-from django.utils.decorators import classproperty
 
 
-class RouteConfig(models.Model):
-    @classproperty
-    def routes(self):
-        return [rt for rt in get_resolver(get_urlconf()).reverse_dict if isinstance(rt, str) and rt[0] != '_']
+def find_routes():
+    from main.urls import urlpatterns
+    return [k.name for k in urlpatterns]
 
+
+class SiteConfig(models.Model):
     is_published = models.BooleanField('indicates weather the Config is published or not', default=False)
-    route = models.CharField(choices=routes, max_length=60)
-    meta_description = models.TextField('Content for the meta with name="description"')
-    title = models.TextField('Content for the head title tag')
-    og_title = models.CharField('Content for the og:title tag', max_length=150)
-    og_description = models.TextField('Content for the og:description tag')
-    og_type = models.CharField('Content for the og:type tag', max_length=50)
-    h1 = models.CharField('Content for the main page title', max_length=100)
-    h3 = models.CharField('Content for the secondary page title', max_length=100)
+    domain = models.CharField(choices=zip(*[settings.ALLOWED_HOSTS]*2), max_length=60)
+
+    def og_fields(self):
+        return dict((field, getattr(self, field)) for field in self._meta.get_fields() if field[:3] == 'og_')
+
+    class Meta:
+        abstract = True
+        ordering = ["is_published"]
 
 
-    @property
-    def short(self):
-        return '%s %s' % (self.tournament.short, self.short_name or self.name)
+class RouteConfig(SiteConfig):
+    route = models.CharField(choices=zip(*[find_routes()]*2), max_length=60)
+    meta_description = models.TextField('Content for the meta with name="description"', blank=True, null=True)
+    title = models.TextField('Content for the head meta title tag(<title>)', blank=True, null=True)
+    og_title = models.CharField('Content for the og:title tag', blank=True, null=True, max_length=150)
+    og_description = models.TextField('Content for the og:description tag', blank=True, null=True)
+    og_type = models.CharField('Content for the og:type tag', blank=True, null=True, max_length=50)
+    h1 = models.CharField('Content for the main page title (h1)', blank=True, null=True, max_length=100)
+    h3 = models.CharField('Content for the secondary page title (h3)', blank=True, null=True, max_length=100)
 
     def __str__(self):
-        return '%s - %s' % (self.route, self.name)
+        return '%s - %s Published: %s' % (self.domain, self.route, self.is_published)
+
+    class Meta:
+        ordering = ["is_published"]
+
+
+class DomainConfig(SiteConfig):
+    og_locale = models.TextField('Content for the og:description tag', blank=True, null=True)
+    og_site_name = models.CharField('Content for the og:type tag', blank=True, null=True, max_length=50)
+    facebook_url = models.CharField('facebook url (page, group or whatever)', blank=True, null=True, max_length=150)
+    twitter_url = models.CharField('url for the twitter Channel', blank=True, null=True, max_length=100)
+    bing_validation_meta = models.CharField('validation code to put into te bing validation meta', blank=True, null=True, max_length=100)
+    google_analytics_script = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return '%s Published: %s' % (self.domain, self.is_published)
 
     class Meta:
         ordering = ["is_published"]
