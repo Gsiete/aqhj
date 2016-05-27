9249ae3ecf50311e798c6ec7a272d2b12d1b5bcb
Gabriel Poussif <gpoussif@gmail.com>
2016-05-27 22:09:45 +0200

    ADD: context processor to inject route_config and domain_config in all templates

--- sites/context_processor.py
+++ sites/context_processor.py
@@ -6,10 +6,13 @@ from sites.models import RouteConfig
 
 # ToDo: remove route_cp when this is ready
 def route_config_cp(request):
-    filter_q = add_check_credentials(request, route=resolve(request.path_info).url_name)
-    route_configs = RouteConfig.objects.filter(filter_q)
+    domain_q = add_check_credentials(request, domain=request.META['HTTP_HOST'])
+    route_q = domain_q & Q(route=resolve(request.path_info).url_name)
+    route_configs = RouteConfig.objects.filter(route_q)
+    domain_configs = RouteConfig.objects.filter(route_q)
     return {
-        'route_config': route_configs[0] if route_configs else None
+        'route_conf': route_configs[0] if route_configs else None,
+        'domain_conf': route_configs[0] if domain_configs else None
     }
 
 
