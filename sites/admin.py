fa44b17dfe079ef1d4983bdd7ed2790372b7fd57
Gabriel Poussif <gpoussif@gmail.com>
2016-05-28 21:14:46 +0200

    CHANGE: enlarge CharField inputs

--- sites/admin.py
+++ sites/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-
+from django.db import models
+from django.forms import TextInput
 from .models import DomainConfig, RouteConfig
 
 
-admin.site.register(RouteConfig)
-admin.site.register(DomainConfig)
+class SitesModelAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': '75'})},
+    }
+
+
+admin.site.register(RouteConfig, SitesModelAdmin)
+admin.site.register(DomainConfig, SitesModelAdmin)
