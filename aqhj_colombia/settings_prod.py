dbece4c6f93c99b48a5cf7723ff7f133eae15620
Gabriel Poussif <gpoussif@gmail.com>
2016-06-03 15:00:39 +0200

    ADD: colombia and mexico sites

---
+++ aqhj_colombia/settings_prod.py
@@ -0,0 +1,9 @@
+from aqhorajuega.settings_prod import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'vsk-u:h4+0$i50pshyol+l84mh(l)&spijpa3!$w%rfqv=06'
+DEBUG = False
+ALLOWED_HOSTS = ['cuandojuegacolombia.com']
+COMPRESS_ENABLED = True
+AQHJ_DOMAIN = 'http://cuandojuegacolombia.com'
+SITE_ID = 2
---
+++ aqhj_mexico/settings_prod.py
@@ -0,0 +1,9 @@
+from aqhorajuega.settings_prod import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'mml-u:g7+0$i50psychl+l84mh(l)&spijpb4!$w%rtqv=06'
+DEBUG = False
+ALLOWED_HOSTS = ['cuandojuegamexico.com']
+COMPRESS_ENABLED = True
+AQHJ_DOMAIN = 'http://cuandojuegamexico.com'
+SITE_ID = 3
