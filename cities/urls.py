48d415f76f4ba2b0485bfe60f4b355587319867f
Gabriel Poussif <gpoussif@gmail.com>
2016-06-08 21:34:44 +0200

    ADD: endpoint to change City Name

--- cities/urls.py
+++ cities/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^ajax/city-autocomplete/$', views.CityAutocomplete.as_view(), name='city-autocomplete'),
+    url(r'^ajax-autocompletar$', views.CityAutocomplete.as_view(), name='city-autocomplete'),
+    url(r'^(?P<slug>[\w-]+)/(?P<country__slug>[\w-]+)$', views.set_city, name='set-city'),
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,5 +1,8 @@
 from dal import autocomplete
+from django.shortcuts import redirect, get_object_or_404
+
 from cities.models import City
+from main.functions import set_cookie
 
 
 class CityAutocomplete(autocomplete.Select2QuerySetView):
@@ -22,3 +25,11 @@ class CityAutocomplete(autocomplete.Select2QuerySetView):
                 'tz': str(result.timezone),
             } for result in context['object_list']
         ]
+
+
+def set_city(request, **kwargs):
+    resp = redirect('index', permanent=True)
+    city = get_object_or_404(City, **kwargs)
+    set_cookie(resp, 'city_code', city.id)
+
+    return resp
