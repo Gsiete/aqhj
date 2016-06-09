fcd73ce2dc15aced326fc5774f8273ed638f40d1
Gabriel Poussif <gpoussif@gmail.com>
2016-06-09 12:55:27 +0200

    REFACTOR: use the Article entities instead of the properties of the match

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, Http404
 from django.utils import timezone
 
 from main.functions import aqhj_render
-from main.models import Match, Season, TeamSeasonGroup
+from main.models import Match, Season, TeamSeasonGroup, Summary, ThreeArticles, Article
 from django.shortcuts import get_object_or_404, redirect
 
 
@@ -14,14 +14,23 @@ def index(request):
     translation.activate(user_language)
     match_filter = add_check_credentials(Q(end_time__gte=timezone.now()), request)
     next_match = Match.objects.filter(match_filter).order_by('time')[0]
-    prev_match_filter = add_check_credentials(Q(end_time__lte=timezone.now()), request)
-    prev_match = Match.objects.filter(prev_match_filter).order_by('-time')[0]
+    article = Article.objects.filter(add_check_credentials(Q(), request, False)).order_by('-created_at').first()
+    summary, three_articles = None, None
+    if article:
+        try:
+            three_articles = article.threearticles
+        except ThreeArticles.DoesNotExist:
+            try:
+                summary = article.summary
+            except Summary.DoesNotExist:
+                pass
 
     fm_filter = match_filter & ~Q(id=next_match.id)
     following_matches = Match.objects.filter(fm_filter).order_by('time')[:6]
 
     return aqhj_render(request, 'main/match-before.html',
-                       {'following_matches': following_matches, 'match': next_match, 'home': True, 'prev_match': prev_match})
+                       {'following_matches': following_matches, 'match': next_match, 'home': True,
+                        'three_articles': three_articles, 'summary': summary})
 
 
 def match_before(request, today=False, **kwargs):
@@ -42,11 +51,13 @@ def match_before(request, today=False, **kwargs):
     if timezone.now() > match.end_time or today and not match.is_today:
         return redirect(match.url, permanent=True)
 
+    three_articles = ThreeArticles.objects.filter(add_check_credentials(Q(match=match), request, False)).first()
     fm_filter = ~Q(id=match.id) & Q(end_time__gte=timezone.now())
     fm_filter = add_check_credentials(fm_filter, request)
     following_matches = Match.objects.filter(fm_filter).order_by('time')[:6]
 
-    return aqhj_render(request, 'main/match-before.html', {'following_matches': following_matches, 'match': match})
+    return aqhj_render(request, 'main/match-before.html',
+                       {'following_matches': following_matches, 'match': match, 'three_articles': three_articles})
 
 
 def past_match(request, **kwargs):
@@ -55,8 +66,11 @@ def past_match(request, **kwargs):
         match = Match.objects.get(match_filter)
     except Match.DoesNotExist:
         raise Http404('No %s matches the given query.' % Match._meta.object_name)
+    summary = Summary.objects.filter(add_check_credentials(Q(match=match), request, False)).first()
+    three_articles = ThreeArticles.objects.filter(add_check_credentials(Q(match=match), request, False)).first()
 
-    return aqhj_render(request, 'main/match-after.html', {'match': match})
+    return aqhj_render(request, 'main/match-after.html',
+                       {'match': match, 'summary': summary, 'three_articles': three_articles})
 
 
 def last_matches(request):
@@ -70,7 +84,6 @@ def redirect_aqhj(request, url):
     return redirect(url, permanent=True)
 
 
-
 def group_round_positions(request, **kwargs):
     # ToDo: try to see if the issue with group by was the name
     season = get_object_or_404(Season, **kwargs)
@@ -79,8 +92,12 @@ def group_round_positions(request, **kwargs):
     return aqhj_render(request, 'main/position-table.html', {'season': season, 'teamstats': teamstats})
 
 
-def add_check_credentials(q_filter, request):
+def add_check_credentials(q_filter, request, for_match=True):
     if not request.user.is_authenticated():
         q_filter &= Q(is_published=True)
-    q_filter &= Q(team_a__site=get_current_site(request)) | Q(team_b__site=get_current_site(request))
+    if for_match:
+        q_filter &= Q(team_a__site=get_current_site(request)) | Q(team_b__site=get_current_site(request))
+    else:
+        q_filter &= Q(site=get_current_site(request))
+
     return q_filter
