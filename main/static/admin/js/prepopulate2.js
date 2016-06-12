8e58cfa05d8b5a0f12dfd099e3fb11668b4be258
Gabriel Poussif <gpoussif@gmail.com>
2016-06-12 19:34:34 +0200

    ADD: Autowrite Name in Short Name field

---
+++ main/static/admin/js/prepopulate2.js
@@ -0,0 +1,47 @@
+(function($) {
+    'use strict';
+    $.fn.prepopulate2 = function(dependencies) {
+        /*
+            Depends on urlify.js
+            Populates a selected field with the values of the dependent fields,
+            URLifies and shortens the string.
+            dependencies - array of dependent fields ids
+            maxLength - maximum length of the URLify'd string
+            allowUnicode - Unicode support of the URLify'd string
+        */
+        return this.each(function() {
+
+            var prepopulatedField = $(this);
+
+            var populate = function() {
+                // Bail if the field's value has been changed by the user
+                if (prepopulatedField.data('_changed')) {
+                    return;
+                }
+
+                var values = [];
+                $.each(dependencies, function(i, field) {
+                    field = $(field);
+                    if (field.val().length > 0) {
+                        values.push(field.val());
+                    }
+                });
+                prepopulatedField.val(values.join(' '));
+            };
+
+            prepopulatedField.data('_changed', false);
+            prepopulatedField.change(function() {
+                prepopulatedField.data('_changed', true);
+            });
+            console.log(dependencies.join(','));
+
+            if (!prepopulatedField.val()) {
+                console.log(dependencies.join(','));
+                $(dependencies.join(',')).keyup(populate).change(populate).focus(populate);
+            }
+        });
+    };
+    $(document).ready(function(){
+        $('.field-short_name input').prepopulate2(['.field-name input']);
+    });
+})(django.jQuery);
