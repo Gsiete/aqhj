b4a79a589b00578012b00a2882a8ff5a8177f369
Gabriel Poussif <gpoussif@gmail.com>
2016-06-12 18:40:38 +0200

    ADD: Filters for the admins

--- main/admin.py
+++ main/admin.py
@@ -2,19 +2,28 @@ from django.contrib import admin
 from .models import Match, Tournament, Team, Stadium, Season, TeamSeason, TeamSeasonGroup, Summary, ThreeArticles
 
 
-class MainAdmin(admin.ModelAdmin):
+class SlugAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
+
+
+class SiteRelAdmin(admin.ModelAdmin):
+    list_filter = ('site',)
+
+
+class SlugSiteRelAdmin(SlugAdmin, SiteRelAdmin):
+    pass
 
 
 class MatchAdmin(admin.ModelAdmin):
     readonly_fields = ('stadium_time', 'url')
+    list_filter = ('season',)
 
 admin.site.register(Match, admin_class=MatchAdmin)
-admin.site.register(Tournament, admin_class=MainAdmin)
-admin.site.register(Team, admin_class=MainAdmin)
+admin.site.register(Tournament, admin_class=SlugAdmin)
+admin.site.register(Team, admin_class=SlugSiteRelAdmin)
 admin.site.register(Stadium)
-admin.site.register(Season, admin_class=MainAdmin)
+admin.site.register(Season, admin_class=SlugAdmin)
 admin.site.register(TeamSeason)
 admin.site.register(TeamSeasonGroup)
-admin.site.register(ThreeArticles)
-admin.site.register(Summary)
+admin.site.register(ThreeArticles, admin_class=SiteRelAdmin)
+admin.site.register(Summary, admin_class=SiteRelAdmin)
