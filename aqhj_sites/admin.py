5fa0fb59e1da2b08afadc8869adf35956f9d87cf
Gabriel Poussif <gpoussif@gmail.com>
2016-06-23 15:35:00 +0200

    ADD: Register redirects in admin

--- aqhj_sites/admin.py
+++ aqhj_sites/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.db import models
 from django.forms import TextInput
-from .models import DomainConfig, RouteConfig
+from .models import DomainConfig, RouteConfig, RedirectOn404
 
 
 class SitesModelAdmin(admin.ModelAdmin):
@@ -12,3 +12,4 @@ class SitesModelAdmin(admin.ModelAdmin):
 
 admin.site.register(RouteConfig, SitesModelAdmin)
 admin.site.register(DomainConfig, SitesModelAdmin)
+admin.site.register(RedirectOn404, SitesModelAdmin)
